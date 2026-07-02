ea367f1ee6761a4aba3a066f87808f3b3eea621d
oughlane <mohammedoughlane72@gmail.com>
2026-07-02 11:17:09 +0100

    test

--- autoload.php
+++ autoload.php
@@ -2,9 +2,9 @@
 spl_autoload_register(function ($class) {
 // chemins possibles selon ton architecture
 $paths = [
-__DIR__ . "./classes/" . $class . ".php",
-__DIR__ . "./interfaces/" . $class . ".php",
-__DIR__ . "./enums/" . $class . ".php"
+__DIR__ . "/classes/" . $class . ".php",
+__DIR__ . "/interfaces/" . $class . ".php",
+__DIR__ . "/enums/" . $class . ".php"
 ];
 });
 foreach ($paths as $file) {
